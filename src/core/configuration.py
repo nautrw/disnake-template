7177044872cb9b5571e5b5b9a7968f8950ded0b3
nautrw <nautrw@proton.me>
2024-06-03 17:28:28 -0400

    feat: create configuration.py

---
+++ src/core/configuration.py
@@ -0,0 +1,5 @@
+configuration = {
+    "token": "", # Your bot's token
+    "test_guilds": [], # A list of integers representing the list of servers as tests
+    "exts": ["src.exts.developer"] # Extensions/cogs for the bot to load
+}
