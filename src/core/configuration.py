778950f3762518b75e0ac72e76cfeed1335dc88a
nautrw <nautrw@proton.me>
2024-06-04 19:00:17 -0400

    refactor: update configuration to include new extensions

--- src/core/configuration.py
+++ src/core/configuration.py
@@ -1,5 +1,5 @@
 configuration = {
     "token": "", # Your bot's token
     "test_guilds": [], # A list of integers representing the list of servers as tests
-    "exts": ["src.exts.developer"] # Extensions/cogs for the bot to load
+    "exts": ["src.exts.developer", "src.exts.example"] # Extensions/cogs for the bot to load
 }
